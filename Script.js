dc31faa6f326aebcaf3fa7eca5c6ee9b7980b886
Loai Mohamed <39069035+loaimohamed@users.noreply.github.com>
2019-03-13 00:53:11 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://raw.githubusercontent.com/loaimohamed/Test/master/Data.json");
 var listM = document.getElementById("listM");
-xhr.onload = function () {
+xhr.onload = async function () {
     var informationZ = JSON.parse(xhr.responseText);
     for (l = 0; l < Object.keys(informationZ).length - 1; l++) {
         var trE = document.createElement("tr");
