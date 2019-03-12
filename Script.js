9e3bb0c771c1b2e2b3de56a92c2c505dcab2feca
Loai Mohamed <39069035+loaimohamed@users.noreply.github.com>
2019-03-13 00:50:28 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -12,26 +12,24 @@ xhr.onload = function () {
         }
         listM.appendChild(trE);
     }
+    ratingsFillter();
 }
 xhr.send();
 function ratingsFillter() {
     listM.firstElementChild.classList.add("topStyle");
     var ageRatings = ["+18", "+17", "+16", "+14"];
-    !function () {
-        var colorSys = document.getElementsByTagName("tr");
-        for (let i = 0; i < colorSys.length; i++) {
-            var mark = document.createElement("span"),
-                listName = colorSys[i].children[0];
-            mark.classList.add("boxmarks");
-            search = colorSys[i].children[6].innerHTML;
-            mark.onclick = function () {}
-            if (search.includes("+12")) { mark.classList.add("T-rating12"); mark.setAttribute("data-color", "+12"); };
-            if (search.includes("+14")) { mark.classList.add("T-rating15"); mark.setAttribute("data-color", "+14"); };
-            if (search.includes("+15")) { mark.classList.add("T-rating15"); mark.setAttribute("data-color", "+15"); };
-            if (search.includes("+16")) { mark.classList.add("T-rating16"); mark.setAttribute("data-color", "+16"); };
-            if (search.includes("+17")) { mark.classList.add("T-rating17"); mark.setAttribute("data-color", "+17"); };
-            if (search.includes("+18")) { mark.classList.add("T-rating18"); mark.setAttribute("data-color", "+18"); };
-            listName.insertAdjacentElement("beforeend", mark);
-        }
-    }();
+    var colorSys = document.getElementsByTagName("tr");
+    for (let i = 0; i < colorSys.length; i++) {
+        var mark = document.createElement("span"),
+            listName = colorSys[i].children[0];
+        mark.classList.add("boxmarks");
+        search = colorSys[i].children[6].innerHTML;
+        if (search.includes("+12")) { mark.classList.add("T-rating12"); mark.setAttribute("data-color", "+12"); };
+        if (search.includes("+14")) { mark.classList.add("T-rating15"); mark.setAttribute("data-color", "+14"); };
+        if (search.includes("+15")) { mark.classList.add("T-rating15"); mark.setAttribute("data-color", "+15"); };
+        if (search.includes("+16")) { mark.classList.add("T-rating16"); mark.setAttribute("data-color", "+16"); };
+        if (search.includes("+17")) { mark.classList.add("T-rating17"); mark.setAttribute("data-color", "+17"); };
+        if (search.includes("+18")) { mark.classList.add("T-rating18"); mark.setAttribute("data-color", "+18"); };
+        listName.insertAdjacentElement("beforeend", mark);
+    }
 }
