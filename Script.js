bbfb8781c9f72bcd5ca47d74935414e581e2d27d
Loai Mohamed <39069035+loaimohamed@users.noreply.github.com>
2019-03-17 17:35:48 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -13,22 +13,24 @@ xhr.onreadystatechange = function () {
         }
         listM.appendChild(trE);
     }
-listM.firstElementChild.classList.add("topStyle");
+    colorSystem();
+}
+xhr.send();
+function colorSystem() {
+    listM.firstElementChild.classList.add("topStyle");
     var ageRatings = ["+18", "+17", "+16", "+14"];
     var colorSys = document.getElementsByTagName("tr");
     for (var i = 0; i < colorSys.length; i++) {
         var mark = document.createElement("span"),
+            search = colorSys[i].children[6].innerHTML,
             listName = colorSys[i].children[0];
         mark.classList.add("boxmarks");
-        search = colorSys[i].children[6].innerHTML;
-        //if (search.includes("+12")) { mark.classList.add("T-rating12"); mark.setAttribute("data-color", "+12"); };
-        if (search.includes("+14")) { mark.classList.add("T-rating15"); mark.setAttribute("data-color", "+14"); };
-        if (search.includes("+15")) { mark.classList.add("T-rating15"); mark.setAttribute("data-color", "+15"); };
-        if (search.includes("+16")) { mark.classList.add("T-rating16"); mark.setAttribute("data-color", "+16"); };
-        if (search.includes("+17")) { mark.classList.add("T-rating17"); mark.setAttribute("data-color", "+17"); };
-        if (search.includes("+18")) { mark.classList.add("T-rating18"); mark.setAttribute("data-color", "+18"); };
+        if (search.includes("+12")) { mark.classList.add("T-rating12"); };
+        if (search.includes("+14")) { mark.classList.add("T-rating15"); };
+        if (search.includes("+15")) { mark.classList.add("T-rating15"); };
+        if (search.includes("+16")) { mark.classList.add("T-rating16"); };
+        if (search.includes("+17")) { mark.classList.add("T-rating17"); };
+        if (search.includes("+18")) { mark.classList.add("T-rating18"); };
         listName.insertAdjacentElement("beforeend", mark);
+    }
 }
-    
-}
-xhr.send();
